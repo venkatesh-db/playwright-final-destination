b021827857a25d3a3e661cb439cf9454dc563968
venkatesh db <54406318+venkatesh-db@users.noreply.github.com>
2025-10-15 13:23:03 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -52,13 +52,14 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    /*
+    
 
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
 
+    /*
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
